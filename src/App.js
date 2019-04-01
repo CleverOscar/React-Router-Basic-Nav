060203c8ce4cd779010f750f965a66ad54f08759
Oscar Pacheco <Oscarpa88@yahoo.com>
2019-04-01 17:32:46 -0500

    Navigation working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
-const App = () => (
-  <div>
-    <Navigation />
-  </div>
-);
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = { }
+  }
+
+  render(){
+    return(
+      <div>
+        <Navigation/>
+        <Route exact path="/" component={ Home }/>
+        <Route exact path="/about" component={ About } />
+        <Route exact path="/contact" component={ Contact } />
+      </div>
+    )
+  }
+}
+
+
 
 export default App;
